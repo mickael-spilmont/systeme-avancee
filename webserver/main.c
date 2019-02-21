3c5f7fbbc2164e75928d958388c7a46c17ef8235
mickael <mickael.spilmont.fr@gmail.com>
2019-02-21 17:54:32 +0100

    Ajout de message de bienvenue envoyé ligne par ligne avec délai

--- webserver/main.c
+++ webserver/main.c
@@ -20,7 +20,7 @@ int bienvenue(int socket_client) {
     }
 
     // On lit tant que le fichier n'est pas vide
-    while (read(fileDescriptor, buffer, 79) > 0) {
+    while (read(fileDescriptor, buffer, 79) != -1) {
         write(socket_client, buffer, strlen(buffer));
         // On vide le buffer
         memset(buffer, 0, 80);
@@ -30,6 +30,37 @@ int bienvenue(int socket_client) {
     return 0;
 }
 
+/* Même chose que bienvenue(), sauf qu'elle lit le fichier ligne et le retourne 
+au client par un nombre de d'appels à la fonction write équivalent au nombre de 
+lignes.
+De plus elle intègre un délais d'une seconde */
+int bienvenueWithDelay(int socket_client) {
+    sleep(1);
+
+    FILE* fichier = fopen("welcome.txt", "r");
+    char* ligne = NULL;
+    size_t tailleLigne = 0;
+
+    // On vérifie que le fichier est correctement ouvert
+    if (fichier == NULL) {
+        perror("Ouverture du fichier");
+        return -1;
+    }
+    
+    // On lit le fichier ligne par ligne avec getline
+    while(getline(&ligne, &tailleLigne, fichier) != -1) {
+        printf("%s", ligne);
+        write(socket_client, ligne, tailleLigne);
+        // On réinitialise la ligne à null
+        ligne = NULL;    
+    }
+    // On ferme le fichier
+    fclose(fichier);
+    return 0;
+}
+
+/* Lit les messages du client et les lui renvoie.
+Prend en paramètre la socket_client et renvoie void */
 void perroquet(int socket_client) {
     char buffer[80] = {0};
 
@@ -52,7 +83,8 @@ int main() {
             return socket_client;
         }
         // On appelle la fonction bienvenue
-        bienvenue(socket_client);
+        // bienvenue(socket_client);
+        bienvenueWithDelay(socket_client);
         // On appelle le perroquet
         perroquet(socket_client);
         // On ferme la socket_client
